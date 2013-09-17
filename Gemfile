2b40a6a76a4a627f292eb449a19f6acde0aaf2e6
Neil Gehani <njgonline@me.com>
2013-09-17 10:01:06 -0700

    Add User Model with Devise and two fields for first and last name

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 gem 'rails', '4.0.0'
 gem 'jquery-rails'
 gem 'devise'
+gem 'simple_form'
 
 group :production do
 	gem 'pg'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  	devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-	attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :as => [:default, :admin]
+  	devise :database_authenticatable, :registerable, #:recoverable
+          :rememberable, :trackable, :validatable
+	attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :provider, :uid, :as => [:default, :admin]
 
 end
