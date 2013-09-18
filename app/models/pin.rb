a1f589e14aa76b45d10ceca9c2a9b0d4124609c5
Neil Gehani <njgonline@me.com>
2013-09-18 16:08:08 -0700

    Add validates :nam to pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,7 @@ class Pin < ActiveRecord::Base
 	attr_accessible :description
 
 	validates :description, presence: true
+	validates :name, presence: true
 
 	belongs_to :user
 	validates :user_id, presence: true
