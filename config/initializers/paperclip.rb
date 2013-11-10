064a09c7ead9544b1d50361d837b5772af1e1bcc
Neil Gehani <njgonline@me.com>
2013-11-10 11:41:32 -0800

    Updated correct paperclip.rb s3_host_name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 'event_sample_images.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'event_sample_images.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
