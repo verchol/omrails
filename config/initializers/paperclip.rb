65c0d5f774fd224e039adeafa559d7703b0c7488
Neil Gehani <njgonline@me.com>
2013-11-10 11:38:38 -0800

    Modified paperclib.rb file

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:s3_host_name] = 'event_sample_images.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
