68914e822f4664538ebb260184e973d0acfb6202
Neil Gehani <njgonline@me.com>
2013-11-13 19:35:34 -0800

    Comment out paperclip s3 host

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 'event_sample_images.s3-website-us-east-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
